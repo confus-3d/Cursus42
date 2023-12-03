a22a5a38dc4822b8369fe2058308798b782e8e12
Francisco <dormilonenlacama@gmail.com>
2023-12-03 16:35:08 +0100

    visual?

--- 02_SO_LONG/prueba.c
+++ 02_SO_LONG/prueba.c
@@ -22,7 +22,7 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
-
+/*
 int	close(int keycode, t_vars *vars)
 {
 	mlx_destroy_window(vars->mlx, vars->win);
@@ -34,21 +34,34 @@ int	key_hook(int keycode, t_vars *vars)
 	printf("Hello from key_hook!\n");
 	return (0);
 }
-
+*/
 int	main(void)
 {
 	t_vars	vars;
 	t_data	img;
+	int		x;
+	int		y;
 
+	x = 0;
+	y = 0;
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(vars.mlx, 1920, 1080);
+	vars.win = mlx_new_window(vars.mlx, 1000, 1000, "Hello world!");
+	img.img = mlx_new_image(vars.mlx, 1000, 1000);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	while (y < 500)
+	{
+		while (x < 500)
+		{
+			my_mlx_pixel_put(&img, x + 5, y + 5, 0x00FF0000);
+			x++;
+		}
+		x = 0;
+		y++;
+	}
 	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
 //	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
-	mlx_key_hook(vars.win, key_hook, &vars);
+//	mlx_key_hook(vars.win, key_hook, &vars);
 	mlx_loop(vars.mlx);
 
 
